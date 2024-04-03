805318425ef2e554234950797056b0c084b10ee4
RaquelTejada <tejadadejesusraquel@gmail.com>
2024-04-03 12:55:02 +0200

    Reactive Forms 2

--- src/app/components/forms/array-form/array-form.component.ts
+++ src/app/components/forms/array-form/array-form.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-array-form',
   templateUrl: './array-form.component.html',
-  styleUrl: './array-form.component.scss'
+  styleUrl: './array-form.component.scss',
 })
-export class ArrayFormComponent {
+export class ArrayFormComponent implements OnInit {
+  myArrayForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myArrayForm = this.formBuilder.group({
+      name: '',
+      surname: '',
+      phones: this.formBuilder.array([]), // Inicializamos la lista de teléfonos vacía.
+    });
+  }
+
+  // Método Getter para obtener el FormArray de la lista de teléfonos
+  get formPhones(): FormArray {
+    return this.myArrayForm.get('phones') as FormArray;
+  }
+
+  // Método para añadir Teléfonos a la lista
+  addPhone() {
+    // Creamos un grupo teléfono
+    const newPhone = this.formBuilder.group({
+      countryCode: '',
+      number: '',
+    });
+
+    // Añadimos el grupo a la lista de teléfonos
+    this.formPhones.push(newPhone);
+  }
+
+  // Método para eliminar teléfonos a la lista
+  deletePhone(index: number) {
+    this.formPhones.removeAt(index);
+  }
 }
--- src/app/components/forms/validated-form/validated-form.component.ts
+++ src/app/components/forms/validated-form/validated-form.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validated-form',
   templateUrl: './validated-form.component.html',
-  styleUrl: './validated-form.component.scss'
+  styleUrl: './validated-form.component.scss',
 })
-export class ValidatedFormComponent {
+export class ValidatedFormComponent implements OnInit {
+  myValidatedForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formBuilder.group({
+      // Campo obligatorio
+      name: ['', Validators.required],
+      // Campo no obligatorio
+      surname: '',
+      // Campo obligatorio con valores mínimos y máximos
+      age: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.min(18),
+          Validators.max(99),
+        ]),
+      ],
+      // Campo obligatorio y de tipo email
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      // Campo obligatorio y con expresión regular
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[a-zA-Z]*'),
+        ]),
+      ],
+      // Campo booleano con true como obligatorio
+      accept: [false, Validators.requiredTrue],
+    });
+  }
+
+  get name() {
+    return this.myValidatedForm.get('name');
+  }
+
+  get surname() {
+    return this.myValidatedForm.get('surname');
+  }
+
+  get age() {
+    return this.myValidatedForm.get('age');
+  }
+
+  get email() {
+    return this.myValidatedForm.get('email');
+  }
+
+  get password() {
+    return this.myValidatedForm.get('password');
+  }
+
+  get accept() {
+    return this.myValidatedForm.get('accept');
+  }
+
+  // Método de Submit del formulario
+  sendForm() {
+    //Controlar que el formulario sea válido
+    if (this.myValidatedForm.valid) {
+      console.table(this.myValidatedForm.value);
+      // Resetear los campos del formulario
+      this.myValidatedForm.reset();
+    }
+  }
 }
